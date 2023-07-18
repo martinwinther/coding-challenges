7b7fcfcfacb4e56a823145c0d7577b0386cee95d
Martin Winther <martin.winther@protonmail.com>
2023-07-18 07:36:50 +0200

    palindromes

--- index.js
+++ index.js
@@ -353,3 +353,35 @@ function reverseStringsInArray(arr) {
 console.log(reverseString(title));
 console.log(reverseStringMan(title));
 console.log(reverseStringsInArray(messages));
+
+/* 
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palindrome, return true. If it isn't, return false.
+
+Example input: "motorbike"
+Example output: false
+
+Example input: "rotator" 
+Example output: true
+*/
+
+// SOLUTION
+
+function isPalindrome(str) {
+	let revStr = str.split("").reverse().join("");
+	if (str === revStr) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
