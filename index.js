064bf7cf963a633c09ee9b938dfb2127de8500e3
Martin Winther <martin.winther@protonmail.com>
2023-07-25 19:34:08 +0200

    some map challenges

--- index.js
+++ index.js
@@ -694,3 +694,126 @@ function findTheWinner(obj) {
 
 console.log(findTheWinner(gameNightFood));
 console.log(findTheWinner(gameNightFood));
+
+/* Totally Private Data Farm 
+
+Good news, renown advertising firm Evil Corp. wants to purchase our 
+private user data! 
+
+We'd never do this in real life of course, but just for practice 
+let's pretend we're unethical web hackers and transform the data 
+in the way Evil Corp. has requested. They're quite particular and
+just want an array of users with a fullname and human readable
+birthday.   
+
+Write a function that maps through the current data and returns
+a new an array of objects with only two properties: 
+fullName and birthday. Each result in your 
+array should look like this when you're done: 
+
+{
+    fullName: "Levent Busser", 
+    birthday: "Fri Aug 20 1971"
+}
+
+Read about toDateString() for info on formatting a readable date. 
+
+*/
+
+import userData from "./data.js";
+
+// SOLUTION
+function transformData(data) {
+	let newArray = [];
+	for (let i = 0; i < data.length; i++) {
+		let fullName = `${data[i].name.first} ${data[i].name.last}`;
+		let birthday = new Date(data[i].dob.date).toDateString();
+		newArray.push({ fullName, birthday });
+	}
+	return newArray;
+}
+
+console.log(transformData(userData));
+
+// SOLUTION 2
+function transformData2(data) {
+	// use map to loop through the data
+	return data.map(({ name, dob }) => {
+		// return an object with the two new properties
+		// concat the first and last name
+
+		// create a new date object, passing in the dob
+		// format by calling toDateString() method
+		return {
+			fullName: `${name.first} ${name.last}`,
+			birthday: new Date(dob.date).toDateString(),
+		};
+	});
+}
+
+console.log(transformData2(userData));
+
+/* Find Free Podcasts 
+
+We have a list of podcasts and need the ability to filter by only
+podcasts which are free.
+
+Write a function that takes in the podcast data and returns an new
+array of only those podcasts which are free.
+
+Additionally, your new array should return only 
+objects containing only the podcast title, rating, and whether or 
+not it is paid. 
+
+Expected output: 
+[
+	{title: "Scrimba Podcast", rating: 10, paid: false}, 
+    {title: "Something about Witches", rating: 8, paid: false}, 
+    {title: "Coding Corner", rating: 9, paid: false}
+]
+*/
+import podcasts from "./data2.js";
+
+// SOLUTION
+
+function getFreePodcasts(data) {
+	const freeArr = data.filter((podcast) => !podcast.paid);
+
+	return freeArr.map((object) => {
+		return { title: object.title, rating: object.rating, paid: object.paid };
+	});
+}
+
+console.log(getFreePodcasts(podcasts));
+
+import products from "./data3.js";
+
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+
+function getSaleItems(data) {
+	return data
+		.filter((snack) => snack.type === "sweet")
+		.map((candy) => {
+			return {
+				item: candy.item,
+				price: candy.price,
+			};
+		});
+}
+
+console.log(getSaleItems(products));
