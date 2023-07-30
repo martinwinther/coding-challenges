9a0e07a71b42a5d58e68b8d35973f49623717a79
Martin Winther <martin.winther@protonmail.com>
2023-07-30 10:10:09 +0200

    reduce and if

--- index.js
+++ index.js
@@ -850,4 +850,26 @@ function total2(arr) {
 }
 
 console.log(total2(shoppingCart));
-5;
+
+import shoppingCart from "./data5.js";
+
+/*
+Use reduce() and only reduce() to calculate and return 
+the total cost of only the savory
+items in the shopping cart.
+
+Expected output: 9.97  
+*/
+
+// SOLUTION
+
+function totalSavory(arr) {
+	return arr.reduce((acc, curr) => {
+		// check if the current item has a type of "savory"
+		// if yes, return acc += curr.price
+		if (curr.type === "savory") return (acc += curr.price);
+		// if no, return acc
+		return acc;
+	}, 0);
+}
+console.log(totalSavory(shoppingCart));
