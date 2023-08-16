6b43718e4b9899bcf0c8cdf9580f1fe2de99d1f3
Martin Winther <martin.winther@protonmail.com>
2023-08-16 19:16:41 +0200

    2 more

--- index.js
+++ index.js
@@ -1132,3 +1132,122 @@ function assignAwards(data) {
 
 console.log(getHosts(podcasts));
 console.log(assignAwards(podcasts));
+
+import podcasts from "./data11.js";
+
+/* 🌴 Save the Weekend 🌴
+
+Your best friend is a copywriter who writes product descriptions 
+for a living. You want to use your hacking skills to help them 
+automate their job so you both can spend the weekend on a 
+tropical island. 
+
+Use array methods and the existing podcast data to write a function that
+can generate a description for each podcast. 
+
+Add the description as a new property on each podcast object, and return
+a new podcast array where each podcast has a description. 
+
+Each description should look like this: 
+[
+    {
+        id: 1,
+        title: "Scrimba Podcast", 
+        ...
+        description: "Scrimba Podcast is a 50 minute education podcast hosted 
+        by Alex Booker."
+    }
+    ...
+]
+
+If the podcast has more than one host, you can display only the first host.
+
+Stretch goal: Display all three hosts in the description, seperated with commas: 
+
+Example description: "Coding Corner is a 55 minute education podcast hosted by Treasure Porth, Guil Hernandez, and Tom Chant."
+*/
+
+// SOLUTION
+
+function createDescriptionsFor(data) {
+	let podcastArr = data.map((data) => {
+		return {
+			...data,
+			description: `${data.title} is a ${data.duration} minute ${data.genre} podcast hosted by ${data.hosts}
+                
+            } 
+            `,
+		};
+	});
+	return podcastArr;
+}
+
+console.log(createDescriptionsFor(podcasts));
+
+/* Find anagrams in an array   
+
+When two words have the exact same letters, they are anagrams. 
+
+Each item in the anagrams array is an anagram of a Scrimba teacher's
+first and last name, plus the phrase "Scrimba teacher". 
+
+Write a function to determine which strings in the array are 
+anagrams of "Bob Ziroll Scrimba Teacher".
+
+Your function should take two parameters: the phrase you want to compare to
+the anagrams, and an array of anagrams. The function should return
+a new array of anagrams that match the phrase. 
+
+Example input: treat, ["tater", "tree", "teart", "tetra", "heart", "hamster"]
+Example output: ["tater", "teart", "tetra"]
+
+Bonus: What other teachers are represented in these anagrams? 
+ */
+
+// SOLUTION
+
+const anagrams = [
+	"moz biblical torchbearers",
+	"it's razorbill beachcomber",
+	"och mcrobbie trailblazers",
+	"bib chorizo cellarmaster",
+	"thor scribble carbimazole",
+	"zilla borscht abercrombie",
+	"brazil scorcher batmobile",
+	"dame shelburne characterizing",
+	"uber englishman characterized",
+	"agnes rhumbline characterized",
+	"rehab scrutinized charlemagne",
+	"dreams zurich interchangeable",
+	"bam hamster technocratic",
+	"mechatronic masterbatch",
+	"bam ratchet mechatronics",
+];
+
+function sortPhrase(phrase) {
+	return phrase.toLowerCase().split("").sort().join("").trim();
+}
+
+function isAnagramInArray(anagram, arr) {
+	// use .filter()
+	// use sortPhrase() to sort both the input phrase and the current phrase in the array
+	// compare the two words to return true or false
+	return arr.filter((item) => {
+		const word1 = sortPhrase(anagram);
+		const word2 = sortPhrase(item);
+
+		return word1 === word2;
+	});
+}
+
+// console.log(isAnagramInArray("Bob Ziroll Scrimba Teacher", anagrams));
+console.log(
+	isAnagramInArray("treat", [
+		"tater",
+		"tree",
+		"teart",
+		"tetra",
+		"heart",
+		"hamster",
+	])
+);
